f1fa74cd6fa5b8945bbaf92492c234961b18c1b1
Apollo Jain <apollojain@Apollos-MBP.home>
2017-06-25 16:05:27 -0400

    added wikipedia integration

--- app.rb
+++ app.rb
@@ -5,18 +5,37 @@ require 'json'
 post '/gateway' do
 	message = params[:text].gsub(params[:trigger_word], '').strip
 
-	action, repo = message.split('_').map {|c| c.strip.downcase }
-	repo_url = "https://api.github.com/repos/#{repo}"
+	action, query = get_qction_query message
 
 	case action
-		when 'issues'
-		  resp = HTTParty.get(repo_url)
-		  resp = JSON.parse resp.body
-		  respond_message "There are #{resp['open_issues_count']} open issues on #{repo}"
+		when 'wiki'
+		  resp = get_wiki query
+		  respond_message resp
+		when 'dictionary'
 	end
 end
 
+def get_action_query message
+	message_arr = message.split(' ')
+	action, query = message[0].strip.downcase, message[1..-1].join(' ')
+	return [action, query]
+end 
+
 def respond_message message
 	content_type :json
-	{:text => message}.to_json
+	{:text => message}.to_json  
 end
+
+def get_wiki search_term
+	search_term = search_term.split.map(&:capitalize).join(' ')
+	hit_url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=#{search_term}"
+	resp = HTTParty.get(hit_url)
+	key = resp["query"]["pages"].keys[0]
+	if key == "-1"
+		return "We are sorry. There are no results for this particular query."
+	else 
+		return resp["query"]["pages"][key]["extract"]
+	end
+
+
+
